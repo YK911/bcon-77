91132367bbb19edddaeda7ffeb63d52f897d57b8
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-09-01 01:23:21 +0300

    fix tasks bug

--- java-script/module-08/lesson-01/js/03-products.js
+++ java-script/module-08/lesson-01/js/03-products.js
@@ -1,11 +1,11 @@
 /**
  * Створи картки з товарами на основі масиву products,
- * приклад картки https://prnt.sc/KmgDlzqOIA3M
+ * приклад картки (./assets/product-card.jpg)
  *
  * Реалізуй делегування подій на колекції карток
  * Після кліку на картку повинно з'являтись модальне вікно
  * з детальною інформацією про продукт,
- * приклад модального вікна https://prnt.sc/vWNoCeZcw7ii
+ * приклад модального вікна (./assets/modal.jpg)
  *
  * Для реалізації модального вікна використай
  * бібліотеку basicLightbox (https://github.com/electerious/basicLightbox)
@@ -14,32 +14,35 @@
 const products = [
   {
     id: 1,
-    img: 'https://www.vodafone.ua/shop/media/wysiwyg/novosti/Capture_1_large.JPG',
-    name: 'Monitor',
-    price: 3000,
-    description: '23-inch monitor with Full HD resolution.',
+    img: 'https://content2.rozetka.com.ua/goods/images/big/546272637.jpg',
+    name: 'Dell SE2425HM',
+    price: 4348,
+    description:
+      'The 100Hz refresh rate provides less flicker, smoother scrolling, and smoother motion.',
   },
   {
     id: 2,
-    img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTzWqRMI3HQiDfICHAmbArmaP4uOOIjfz0sDITv0dfkpb0mbbgX',
-    name: 'Laptop',
-    price: 20000,
+    img: 'https://content2.rozetka.com.ua/goods/images/big/575063470.jpg',
+    name: 'Samsung Odyssey G50D',
+    price: 10799,
     description:
       'Lightweight and powerful laptop with a 15-inch display and SSD.',
   },
   {
     id: 3,
-    img: 'https://cdn.27.ua/799/66/39/6841913_1.jpeg',
-    name: 'Smartphone',
-    price: 8000,
-    description: 'Equipped with a triple camera and a multi-core processor.',
+    img: 'https://content2.rozetka.com.ua/goods/images/big/483286979.jpg',
+    name: 'Asus VG249QL3A',
+    price: 5899,
+    description:
+      'The TUF Gaming VG249QL3A gaming monitor features a 23.8-inch Fast IPS panel.',
   },
   {
     id: 4,
-    img: 'https://cdn.27.ua/799/b6/16/4371990_1.jpeg',
-    name: 'Tablet',
-    price: 12000,
-    description: '10-inch tablet with high performance and a Retina display.',
+    img: 'https://content2.rozetka.com.ua/goods/images/big/419088411.jpg',
+    name: 'MSI MAG 274QRF',
+    price: 10899,
+    description:
+      'The MAG 274QRF QD E2 features a WQHD panel with a resolution of 2560x1440.',
   },
 ];
 
