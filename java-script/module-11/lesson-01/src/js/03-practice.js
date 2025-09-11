9af256ef0426831d9676274c2bf411eb596c7217
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-09-11 13:19:12 +0300

    finished module 11 lesson 01

--- java-script/module-11/lesson-01/src/js/03-practice.js
+++ java-script/module-11/lesson-01/src/js/03-practice.js
@@ -5,10 +5,37 @@ import '../common.css';
  * Використовуємо https://pokeapi.co/ та створимо сторінку перегляду покемонів
  */
 
-function fetchPokemon(pokemonId) {}
-
+const BASE_URL = 'https://pokeapi.co/api/v2';
 const cardContainer = document.querySelector('.card-container');
 const searchForm = document.querySelector('.search-form');
+
+searchForm.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const searchQuery = form.elements.searchQuery.value.toLowerCase();
+
+  fetchPokemon(searchQuery)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+}
+
+function fetchPokemon(pokemonId) {
+  const params = new URLSearchParams();
+  const headers = new Headers();
+  const url = `${BASE_URL}/pokemon/${pokemonId}`;
+
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.message);
+    }
+
+    return response.json();
+  });
+}
 
 function renderPokemonCard({ name, sprites, weight, height, abilities }) {
   const abilityListItems = abilities
---
+++ java-script/module-11/lesson-01/src/js/config.js
@@ -0,0 +1,3 @@
+export const API_KEY =
+  'live_l04vokWs8d2RAPJS5bi3tkFB1jk75SXGVi8XKPG0BFZf5RWuSRjlK9YnvK3pgUEF';
+export const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
