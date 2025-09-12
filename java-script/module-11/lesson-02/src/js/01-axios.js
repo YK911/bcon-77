5fb9dea9a211b6489a28c3019c93e424037d4739
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-09-12 13:40:42 +0300

    finished module 11 lesson 02

--- java-script/module-11/lesson-02/src/js/01-axios.js
+++ java-script/module-11/lesson-02/src/js/01-axios.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import '../common.css';
 
 /**
@@ -11,24 +12,56 @@ import '../common.css';
 
 const list = document.querySelector('.todo-list');
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    // Рендер елементів в DOM (map і insertAdjacentHTML)
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//     // Рендер елементів в DOM (map і insertAdjacentHTML)
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
 
 /**
  * Всередині функції запит, зовні обробка
  */
-const fetchTodos = () => {};
+// const fetchTodos = () => {
+//   return axios({
+//     method: 'GET',
+//     url: 'https://jsonplaceholder.typicode.com/todos/10001',
+//   });
+// };
+const selectEl = document.querySelector('.js-select');
+// const selectedValue = +selectEl.selectedOptions[0].value;
+const selectedValue = Number(selectEl.selectedOptions[0].value);
 
-// fetchTodos().then().catch()
+const fetchTodos = limit => {
+  const options = {
+    params: {
+      _limit: limit,
+      // _sort: 'title',
+    },
+    headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  };
+
+  return axios.get('https://jsonplaceholder.typicode.com/todos', options);
+};
+const renderTodos = ({ data }) => {
+  console.log(data);
+};
+const handleError = error => {
+  console.log(error.message);
+};
+
+fetchTodos(selectedValue).then(renderTodos).catch(handleError);
+
+selectEl.addEventListener('change', event => {
+  const selectedValue = +event.target.selectedOptions[0].value;
+  // console.log('🚀 ~ selectedValue:', selectedValue);
+  fetchTodos(selectedValue).then(renderTodos).catch(handleError);
+});
